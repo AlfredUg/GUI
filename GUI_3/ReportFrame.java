3c0b5c8f97c29c2100818294d1d7ea9ad5b73fc4
Alfred Ssekagiri <assekagiri@gmail.com>
2017-07-20 23:32:20 +0100

    Delete ReportFrame.java

--- GUI_3/ReportFrame.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import javax.swing.*;
-
-/**
- * Class to define window in which attendance report is displayed.
- */
-public class ReportFrame extends JFrame {
-        // your code here
-}
