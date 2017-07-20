24b4a75043cea903c9ff353ea8d7b9f29306b69d
Alfred Ssekagiri <assekagiri@gmail.com>
2017-07-20 23:32:05 +0100

    Delete FitnessClass.java

--- GUI_3/FitnessClass.java
+++
@@ -1,9 +0,0 @@
-/** Defines an object representing a single fitness class
- */
-public class FitnessClass implements Comparable<FitnessClass> {
-    // your code here
-
-    public int compareTo(FitnessClass other) {
-	  return 0; // replace with your code
-    }
-}
